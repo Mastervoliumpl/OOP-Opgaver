398c72780db8a5d265afba652415de8f18b72767
Jakub Fijalkowski <jakfi0001@elevcampus.dk>
2023-12-19 14:47:10 +0100

    Bog5 done

--- Bog5/Bog5/Boghanlder.cs
+++ Bog5/Bog5/Boghanlder.cs
@@ -12,5 +12,13 @@ namespace Bog5
         {
             bogMethod.str = "Such cool book to read you should read it is really cool I really like it and you should too.";
         }
+
+        public void Indlæs(out string name, out string address)
+        {
+            Console.Write("Indtast din navn: ");
+            name = Console.ReadLine();
+            Console.Write("Indtast din adresse: ");
+            address = Console.ReadLine();
+        }
     }
 }
--- Bog5/Bog5/Program.cs
+++ Bog5/Bog5/Program.cs
@@ -14,7 +14,28 @@
 
             handler.Sælg(bog);
 
+            string customerName;
+            string customeraddress;
+
+            handler.Indlæs(out customerName, out customeraddress);
+
+            Console.WriteLine("----------");
+            Console.WriteLine($"Kundes navn: {customerName} | Kundes adresse: {customeraddress}");
+            Console.WriteLine("----------");
             Console.WriteLine($"Title: {bog.str} | Antal sider: {bog.antalSider}");
+
+            Console.WriteLine("---------------------------------------");
+            Console.Write("Indtast en kommatal: ");
+            string userinput = Console.ReadLine();
+            if (double.TryParse(userinput, out double doubleValue))
+            {
+                Console.WriteLine($"Number could be Parsed: {doubleValue}");
+            }
+            else 
+            {
+                Console.WriteLine($"Unable to Parse number: {doubleValue}");
+            }
+
         }
     }
 }
