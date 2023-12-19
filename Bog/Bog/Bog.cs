8edecd60cdc80655bdb8bc2cc20d616dac686860
Jakub Fijalkowski <jakfi0001@elevcampus.dk>
2023-12-19 10:06:57 +0100

    Add project files.

---
+++ Bog/Bog/Bog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bog
+{
+    class Bog
+    {
+        public string title { get; set; }
+        public string author {  get; set; }
+    }
+}
---
+++ Bog/Bog/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Bog
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Bog bog = new Bog();
+            Console.Write("Title: ");
+            bog.title = Console.ReadLine();
+            Console.Write("Author: ");
+            bog.author = Console.ReadLine();
+
+            Console.WriteLine($"Title: {bog.title} | Author: {bog.author}");
+        }
+
+        
+    }
+}
