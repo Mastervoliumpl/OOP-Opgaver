1fb16a24b22a9780b4d2d28f3acf592e3b84406f
Jakub Fijalkowski <jakfi0001@elevcampus.dk>
2023-12-19 11:16:36 +0100

    Imported Bog2 project

---
+++ Bog3/Bog3/Bog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+namespace Bog2
+{
+    class Bog
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+
+        public Bog(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+    }
+}
---
+++ Bog3/Bog3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Bog2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+            Console.Write("Author: ");
+            string author = Console.ReadLine();
+
+            Bog bog = new Bog(title, author);
+            Bog bog2 = new Bog(title, author)
+            {
+                title = "Test Bog Title",
+                author = "Test Bog Author"
+            };
+
+            Console.WriteLine($"Title: {bog.title} | Author: {bog.author}");
+            Console.WriteLine($"Title: {bog2.title} | Author: {bog2.author}");
+        }
+    }
+}
