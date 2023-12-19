3ee256a2425fa64a803adf163bed3454630c4602
Jakub Fijalkowski <jakfi0001@elevcampus.dk>
2023-12-19 12:55:16 +0100

    Bog 4 Done

---
+++ Bog4/Bog4/Boghandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bog4
+{
+    public class Boghandler
+    {
+        public void Sælg(Bog bogMethod) 
+        {
+            bogMethod.str = "Gay Hitler Sex";
+        }
+    }
+}
---
+++ Bog4/Bog4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Bog4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Bog bog = new Bog
+            {
+                str = "Hello",
+                antalSider = 17
+            };
+
+            Boghandler handler = new Boghandler();
+
+            handler.Sælg(bog);
+
+            Console.WriteLine($"Title: {bog.str} | Antal sider: {bog.antalSider}");
+        }
+    }
+}
