8d9a0b53a509b05fe20aec83a90a79cc053e7e8c
Mastervoliumpl <kubafijalkowski25@gmail.com>
2023-12-20 23:31:55 +0100

    Bog13 Done

---
+++ Bog13/Bog13/Bil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bog13
+{
+    public struct Bil
+    {
+        public int Km;
+        public int Hk;
+        public string Farve;
+        
+
+        public Bil(string farve, int km, int hk)
+        {
+            Km = km;
+            Hk = hk;
+            Farve = farve;
+        }
+
+        public void Udskriv()
+        {
+            Console.WriteLine($"Farve: {Farve}, Km: {Km}, HK: {Hk}");
+        }
+    }
+}
---
+++ Bog13/Bog13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Bog13
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Oprettelse af en bil med new
+            Bil bil1 = new Bil("Blue", 50000, 150);
+            bil1.Udskriv();
+
+            Bil bil2 = new Bil { Farve = "Orange", Km = 25000, Hk = 100 };
+            bil2.Udskriv();
+
+            Bil bil3 = bil2;
+            bil3.Farve = "Yellow";
+
+            bil2.Udskriv();
+            bil3.Udskriv();
+        }
+    }
+}
