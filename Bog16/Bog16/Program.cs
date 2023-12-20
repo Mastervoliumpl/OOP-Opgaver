39bab9181a77dab98083236e9c6286ac192410da
Mastervoliumpl <kubafijalkowski25@gmail.com>
2023-12-20 23:49:24 +0100

    Bog16 Done

---
+++ Bog16/Bog16/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Bog16
+{
+    public delegate int CalculatorDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main()
+        {
+            CalculatorDelegate calculator = null;
+
+            Console.WriteLine("Vælg operation:");
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraktion");
+            string valg = Console.ReadLine();
+
+            switch (valg)
+            {
+                case "1":
+                    calculator = Add;
+                    break;
+                case "2":
+                    calculator = Sub;
+                    break;
+                default:
+                    Console.WriteLine("Ugyldigt valg.");
+                    return;
+            }
+
+            Console.WriteLine("Indtast første tal:");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast andet tal:");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultat = calculator(tal1, tal2);
+            Console.WriteLine($"Resultat: {resultat}");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
