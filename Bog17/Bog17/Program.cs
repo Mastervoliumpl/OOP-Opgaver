3efbd4fbd5b3c6d79ea3890f74528e750bc1b523
Mastervoliumpl <kubafijalkowski25@gmail.com>
2023-12-20 23:55:34 +0100

    Bog17 Done

---
+++ Bog17/Bog17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bog17
+{
+    delegate int MyDelegate(int p, int q);
+
+    class Program
+    {
+        static void Main()
+        {
+            MyDelegate arithMethod = delegate (int c, int d)
+            {
+                return c > d ? c : d;
+            };
+
+            Console.WriteLine("Det største af tallene er: {0}", arithMethod(3, 7));
+        }
+    }
+}
