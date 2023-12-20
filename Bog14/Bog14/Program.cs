5fff5d9645d2f5ce2361e121706ccabae580ed90
Mastervoliumpl <kubafijalkowski25@gmail.com>
2023-12-20 23:39:06 +0100

    Bog14 Done

---
+++ Bog14/Bog14/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bog14
+{
+    public enum Colors
+    {
+        Purple,
+        Green,
+        Yellow,
+        Blue,
+        Red
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Colors myColor = Colors.Blue;
+            Udskriv(myColor);
+        }
+
+        static void Udskriv(Colors color)
+        {
+            Console.WriteLine($"Chosen color: {color}");
+        }
+    }
+}
