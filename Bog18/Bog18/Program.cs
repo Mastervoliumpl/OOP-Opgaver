1510f7d3150d2b223e0cc84049c90f6b10e84297
Mastervoliumpl <kubafijalkowski25@gmail.com>
2023-12-21 00:06:01 +0100

    Bog18 Done

---
+++ Bog18/Bog18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Timers;
+
+namespace Bog18
+{
+    class Program
+    {
+        static void Main()
+        {
+            var timer = new System.Timers.Timer(5000) 
+            { 
+                AutoReset = true, 
+                Enabled = true 
+            };
+
+            ConsoleColor[] farver = new[]
+            {
+                ConsoleColor.Red,
+                ConsoleColor.Green,
+                ConsoleColor.Blue,
+                ConsoleColor.Yellow,
+                ConsoleColor.Cyan
+            };
+
+            timer.Elapsed += (sender, e) =>
+            {
+                var farveIndex = (DateTime.Now.Second / 5) % farver.Length;
+                Console.BackgroundColor = farver[farveIndex];
+                Console.Clear();
+            };
+
+            Console.WriteLine("Tryk 'Enter' for at afslutte programmet.");
+            Console.ReadLine();
+        }
+    }
+}
