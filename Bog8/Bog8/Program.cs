e525b07013cd49919f882ae4d6bd92a45e941611
Jakub Fijalkowski <jakfi0001@elevcampus.dk>
2023-12-20 08:24:38 +0100

    Bog8 Done

---
+++ Bog8/Bog8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bog8
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{i * j}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
